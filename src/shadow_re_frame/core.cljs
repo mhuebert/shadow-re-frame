f1acf98b19d32f75edbba1b5ab514d894e8d7354
Matthew Huebert <no-email@gmail.com>
2017-10-21 14:45:05 +0200

    Basic example

--- src/shadow_re_frame/core.cljs
+++ src/shadow_re_frame/core.cljs
@@ -1,109 +1,80 @@
 (ns shadow-re-frame.core
   (:require
-    [re-view.core :as v :refer [defview]]
-    [shadow-re-frame.examples :as examples]
-    [re-frame.core :as re]
-    [reagent.core :as reagent]
-    ))
-
-(enable-console-print!)
-
-(comment
-
-  (defview text
-    "A simple view element with text, using the :name prop."
-    [this]
-    [:div {:style {:font-size  30
-                   :text-align "center"}}
-     "Welcome to " (:name this) "!"])
-
-  (defview counter
-    "Example of using the :view/state atom to keep local state."
-    [this]
-    [:div
-     {:on-click #(swap! (:view/state this) inc)
-      :style    {:padding    10
-                 :background "#eee"
-                 :cursor     "pointer"}}
-     "I have been clicked " (or @(:view/state this) 0) " times."])
+    [re-frame.core :as rf]
+    [reagent.core :as reagent]))
 
 
-  (def spacer [:div {:style {:height 10}}])
+;; 1. Event Dispatch
+;;    make a view, dispatch an event in a click handler
 
-  (defview layout [this]
-    [:div
-     {:style {:max-width 300
-              :margin    "50px auto"
-              :font-size 16}}
-     (text {:name "shadow-re-frame"})
-     spacer
-     (counter)
-     spacer
-     (examples/custom-cursor)
-     spacer
-     (examples/todo-list)])
-
-
-
-  (defn ^:export render []
-    (v/render-to-dom (layout) "shadow-re-frame")))
-
-;; initialize
-(re/reg-event-db
-  :initialize
-  (constantly {:counters {"A" 0
-                          "B" 0
-                          "C" 0}}))
-
-;; The six dominoes are:
-;;
-;; Event dispatch - something happens - the user clicks a button, or a websocket receives a new message.
-;; Event handling - Event handler functions compute a description of effects
-;; Effect handling - descriptions of effects are realised (actioned)
-;; Query -  extracting data from "app state", and providing it in the right format for view functions. de-duplicated signal graph.
-;; View - one or more view functions (aka Reagent components) compute hiccup-formatted data
-;; DOM - handled for you by Reagent/React
-
-
-;; View + event Dispatch
 (defn count-button
-  [[counter-id counter-value]]
-  [:div {:key      counter-id
-         :on-click #(re/dispatch [:inc-counter counter-id])
-         :style    {:padding    20
-                    :margin     10
-                    :background "rgba(0,0,0,0.05)"}}
-   counter-value]
-  )
+  "Given a counter id and its current value, render it as an interactive widget."
+  [counter-id]
+  (let [counter-value @(rf/subscribe [:counter counter-id])]
+    [:div {:key      counter-id
+           :on-click #(rf/dispatch [:inc-counter counter-id])
+           :style    {:padding    20
+                      :margin     10
+                      :background "rgba(0,0,0,0.05)"}}
+     counter-value]))
 
-;; Event handling
-(re/reg-event-fx :inc-counter
-                 (fn [{:keys [db] :as coeffects} [_ counter-id]]
-                   {:db (update-in db [:counters counter-id] (fnil inc 0))}))
 
-(comment
-  ;; re/reg-event-db passes `db` instead of the 'coeffects' map
-  (re/reg-event-db :inc-counter
-                   (fn [db [_ counter-id]]))
-  )
+;; 2. Event Handling
+;;    register a handler for a given event.
+;;
+;;    - handlers are identified by keyword.
+;;    - simple method:   `reg-event-db` is passed `db` as 1st argument.
+;;      advanced method: `reg-event-fx` is passed 'co-effects' map as 1st argument of which `:db` is one key.
 
-;; Query - reg-sub associates a query id with a function of db
+(rf/reg-event-db :inc-counter
+                 (fn [db [_ counter-id]]
+                   (update-in db [:counters counter-id] inc)))
 
-(re/reg-sub :read-counters
-            (fn query-fn
-              [db [_ _]]
-              (get db :counters)))
 
-;; View
+(rf/reg-event-db :initialize
+                 ;; we'll call this once, at the beginning, to set up the db.
+                 (constantly {:counters {"A" 0
+                                         "B" 0
+                                         "C" 0}}))
+
+;; 3. Queries
+;;    make a query for every kind of 'read' into the db.
+;;
+;;    - queries are identified by keyword.
+;;    - queries can (optionally) take parameters.
+;;    - `db` is passed as 1st arg to function.
+;;      vector of [query-id & args] is passed as 2nd arg.
+
+(rf/reg-sub :counter
+            (fn [db [_ counter-id]]
+              (get-in db [:counters counter-id])))
+
+(rf/reg-sub :counter-list
+            (fn [db _]
+              (-> (get db :counters)
+                  (keys))))
+
+;; 4. Views
+;;    Use reagent to create views.
+;;
+;;    - use `re-frame.core/subscribe` to read queries
+;;    - use `reagent/atom` for local state (not shown here)
 
 (defn root-view
+  "Render the page"
   []
   [:div
-   (let [counters (re/subscribe [:read-counters])]
-     (map count-button @counters))])
+   {:style {:max-width 300
+            :margin    "50px auto"
+            :font-size 16}}
+   (let [counters (rf/subscribe [:counter-list])]
+     (doall (map count-button @counters)))])
 
 (defn ^:export render []
-  (re/dispatch-sync [:initialize])
   (reagent/render [root-view]
                   (js/document.getElementById "shadow-re-frame")))
+
+(defn ^:export init []
+  (rf/dispatch-sync [:initialize])
+  (render))
 
